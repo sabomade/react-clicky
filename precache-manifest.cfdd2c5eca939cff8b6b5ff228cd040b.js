0e36c31b7fd862d4e2d018d182ce11d021950c8c
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-03-04 18:12:56 -0800

    Updates

---
+++ precache-manifest.cfdd2c5eca939cff8b6b5ff228cd040b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcd7ddfcc5542c3c052b3c502557b54",
+    "url": "/react-clicky/index.html"
+  },
+  {
+    "revision": "ff5a7724ca1d5e8387ca",
+    "url": "/react-clicky/static/css/main.9eae0410.chunk.css"
+  },
+  {
+    "revision": "f838697e55fe7a38074c",
+    "url": "/react-clicky/static/js/2.81fde50c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-clicky/static/js/2.81fde50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5a7724ca1d5e8387ca",
+    "url": "/react-clicky/static/js/main.b07363a0.chunk.js"
+  },
+  {
+    "revision": "97cfcf2d69bac71400f2",
+    "url": "/react-clicky/static/js/runtime-main.ef7dcd2c.js"
+  }
+]);
